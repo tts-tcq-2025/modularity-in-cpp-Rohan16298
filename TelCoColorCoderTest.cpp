798e1ad606e52d46437e5f569c94f1fc8c48dfdf
Rohan16298 <urk3hyd@bosch.com>
2025-09-29 12:00:26 +0530

    Update TelCoColorCoderTest.cpp

--- TelCoColorCoderTest.cpp
+++ TelCoColorCoderTest.cpp
@@ -15,11 +15,11 @@ void testPairToNumber(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorC
     assert(pairNumber == expectedPairNumber);
 }
 
-int main() {
-    testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
-    testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
-    testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
-    testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
-    std::cout << "All tests passed successfully." << std::endl;
-    return 0;
-}
+//int main() {
+   // testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
+   // testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
+   // testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
+  //  testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
+  //  std::cout << "All tests passed successfully." << std::endl;
+  //  return 0;
+//}
